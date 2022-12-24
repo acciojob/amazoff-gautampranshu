f61d891bd1954c14e7d6a2f82e8225ce27b4c48d
pranshu <pranshugautam1998@gmail.com>
2022-12-24 16:08:28 +0530

    commit 4

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -110,8 +110,8 @@ public class OrderController {
 
     @GetMapping("/get-last-delivery-time/{partnerId}")
     public ResponseEntity<String> getLastDeliveryTimeByPartnerId(@PathVariable String partnerId){
-        String time = null;
-
+       // String time = null;
+            String time = "18:00";
         //Return the time when that partnerId will deliver his last delivery order.
 
         return new ResponseEntity<>(time, HttpStatus.CREATED);
